d2c7308d4035b54788e2ad53a7b2d0cebb1e3b45
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-05-21 22:57:50 +0530

    Implicit Razor Expression "@"

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random rand = new Random();
+            int randNum = rand.Next(0, 2);
+            IsOnline = randNum == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
