8758546b11e501dba7f67868be6c0abba8054b34
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-05-22 22:24:41 +0530

    Interactive Server Config

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -3,7 +3,7 @@ using TodoApp.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -20,6 +20,6 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
