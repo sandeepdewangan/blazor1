7d743194a619985a4a45d5de46fb2406a5ca07c7
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-05-22 22:43:21 +0530

    TodoItems Repo

---
+++ TodoApp/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+        public DateTime DateCompleted { get; set; }
+    }
+}
---
+++ TodoApp/Models/TodoItemsRepo.cs
@@ -0,0 +1,63 @@
+﻿namespace TodoApp.Models
+{
+    public class TodoItemsRepo
+    {
+        private static List<TodoItem> items = new List<TodoItem>()
+        {
+            new TodoItem {Id = 1, Name = "Item 1"},
+            new TodoItem {Id = 2, Name = "Item 2"},
+            new TodoItem {Id = 3, Name = "Item 3"},
+        };
+
+        public static void AddItem(TodoItem item)
+        {
+            if (items.Count > 0)
+            {
+                var maxId = items.Max(s => s.Id);
+                item.Id = maxId + 1;
+                items.Add(item);
+            }
+            else
+            {
+                item.Id = 1;
+                items.Add(item);
+            }
+        }
+
+        public static List<TodoItem> GetItems() => items;
+
+        public static TodoItem? GetItemById(int Id)
+        {
+            TodoItem? item = items.FirstOrDefault(s => s.Id == Id);
+            if (item != null)
+            {
+                return new TodoItem { Id = item.Id, Name = item.Name };
+            }
+            return null;
+        }
+
+        public static void UpdateItem(int itemId, TodoItem item)
+        {
+            if (itemId != item.Id) return;
+            var itemToUpdate = items.FirstOrDefault(s => s.Id == itemId);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Name = item.Name;
+            }
+        }
+
+        public static void DeleteItem(int id)
+        {
+            var item = items.FirstOrDefault(s => s.Id == id);
+            if (item != null)
+            {
+                items.Remove(item);
+            }
+        }
+        public static List<TodoItem> SearchItems(string itemFilter)
+        {
+            return items.Where(s => s.Name.Contains(itemFilter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+    }
+}
