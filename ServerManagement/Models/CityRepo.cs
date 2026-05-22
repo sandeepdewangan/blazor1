4a95cae68ccc0763a6f64734d43b84bbee1cfee1
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-05-22 10:42:32 +0530

    Looping through cities

---
+++ ServerManagement/Models/CityRepo.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public class CityRepo
+    {
+        private static List<String> cities = new List<String>()
+        {
+            "Raipur",
+            "Bilaspur",
+            "Kanker",
+            "Dhamtari",
+            "Balod",
+            "Surguja",
+        };
+        public static List<String> GetCities() => cities;
+
+    }
+}
